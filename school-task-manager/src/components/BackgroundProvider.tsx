cb34d349a954628ddb80b18181b67aa14783e000
Ly-Prum <y.29adachi24@gmail.com>
2026-05-13 10:38:15 +0900

    fix: use style tag injection for background image on mobile

--- school-task-manager/src/components/BackgroundProvider.tsx
+++ school-task-manager/src/components/BackgroundProvider.tsx
@@ -1,35 +1,28 @@
 'use client'
-import { useEffect } from 'react'
 import { useStore } from '@/lib/store'
 
 export default function BackgroundProvider() {
   const bgImage = useStore(s => s.bgImage)
-
-  useEffect(() => {
-    const body = document.body
-    if (bgImage) {
-      body.style.backgroundImage = `url('${bgImage}')`
-      body.style.backgroundSize = 'cover'
-      body.style.backgroundPosition = 'center center'
-      body.style.backgroundRepeat = 'no-repeat'
-      body.style.backgroundAttachment = 'scroll'
-    } else {
-      body.style.backgroundImage = ''
-      body.style.backgroundSize = ''
-      body.style.backgroundPosition = ''
-      body.style.backgroundRepeat = ''
-      body.style.backgroundAttachment = ''
-    }
-  }, [bgImage])
-
   if (!bgImage) return null
+
   return (
-    <div style={{
-      position: 'fixed',
-      top: 0, left: 0, right: 0, bottom: 0,
-      zIndex: 1,
-      pointerEvents: 'none',
-      background: 'rgba(8,8,11,0.55)',
-    }} />
+    <>
+      <style>{`
+        html, body {
+          background-image: url('${bgImage}') !important;
+          background-size: cover !important;
+          background-position: center center !important;
+          background-repeat: no-repeat !important;
+          background-attachment: scroll !important;
+        }
+      `}</style>
+      <div style={{
+        position: 'fixed',
+        top: 0, left: 0, right: 0, bottom: 0,
+        zIndex: 1,
+        pointerEvents: 'none',
+        background: 'rgba(8,8,11,0.55)',
+      }} />
+    </>
   )
 }
